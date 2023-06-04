230678318276ee8c8d6ae156a1d7353318a8caaf
ksh_8 <ksh_0403@naver.com>
2023-06-05 03:02:27 +0900

    [design] image intent & result activity design

--- app/src/main/java/com/example/gardengenie_android/PhotoResultActivity.java
+++ app/src/main/java/com/example/gardengenie_android/PhotoResultActivity.java
@@ -5,6 +5,8 @@ import androidx.annotation.Nullable;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -12,5 +14,24 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+
 public class PhotoResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_result);
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+        byte[] byteArray = getIntent().getByteArrayExtra("image");
+        Bitmap bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+
+        imageView.setImageBitmap(bitmap);
+
+    }
+
+
+
+
 }
