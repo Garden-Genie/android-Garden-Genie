d46e4e0054afa4add7753d1c7471a3d2a483ef0f
ksh_8 <ksh_0403@naver.com>
2023-06-14 18:28:33 +0900

    [fix] add URL file

--- app/src/main/java/com/example/gardengenie_android/BaseUrl.java
+++ app/src/main/java/com/example/gardengenie_android/BaseUrl.java
@@ -3,9 +3,9 @@ package com.example.gardengenie_android;
 public class BaseUrl {
 
     // your-server-url
-    public static final String BASE_URL = "http://your-server-url";
+    public static final String BASE_URL= "http://192.168.45.97:8070";
 
     // flask-server-url
-    public static final String FLASK_URL = "http://flask-server-url";
+    public static final String FLASK_URL= "http://34.64.146.226:5000/analyze";
 
 }
